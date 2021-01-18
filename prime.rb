0f5315bfc9e0bc213ad06095004b4e613268091d
James Harris <mharris9608@yahoo.com>
2021-01-18 14:49:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(x)
+  if x == 0 || x == 1
+    return false
+  elsif (2..value - 1).each do |i|
+    if value % i == 0
+      return false
+    end
+  end
+end
+true
+end
+    
