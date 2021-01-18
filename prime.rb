3b11f48fc585b67371e9882dbbcfa2d9c6e559a4
James Harris <mharris9608@yahoo.com>
2021-01-18 14:50:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,11 @@
 def prime?(x)
   if x == 0 || x == 1
     return false
-  elsif (2..value - 1).each do |i|
-    if value % i == 0
+  elsif (2..x - 1).each do |i|
+    if x % i == 0
       return false
     end
   end
 end
 true
 end
-    
